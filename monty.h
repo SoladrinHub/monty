cef63f6299e50eb98f9811073687392c7609a06a
Sear5566 <dwdsegun63@gmail.com>
2023-12-15 01:25:02 +0000

    added more structures to the monty.h file

--- monty.h
+++ monty.h
@@ -40,7 +40,19 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/*void push(stack_t **stack, unsigned int line_number);*/
+/**
+ * struct arg - global variable
+ * @line: pointer
+ * Description: global variable
+*/
+typedef struct arg
+{
+	char **line;
+}global_t;
+global_t global;
+
+//void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif
